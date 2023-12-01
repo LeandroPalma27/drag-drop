c91556ef7ec24c49973b9e36b48b3f13184a61a3
LeandroPalma27 <psychomusicbox@gmail.com>
2023-12-01 14:38:50 -0500

    Avances en el envio de archivos desde el frontend (con exito)

--- src/js/provider/functions/uploadFiles.js
+++ src/js/provider/functions/uploadFiles.js
@@ -2,22 +2,20 @@ import { URL } from "../url-cloudinary-spring-restfulservice";
 
 export function subirArchivos(archivosCargados) {
     const onlyFiles = archivosCargados.map(e => e.file);
-    const formData  = new FormData();
-    for(const file of onlyFiles) {
-        formData.append(file.name, file);
-      }
-    
+    const formData = new FormData();
+    for (const file of onlyFiles) {
+        formData.append("files", file);
+    }
+
     fetch(URL.concat('/upload'), {
         method: "POST", 
         mode: "cors",
         cache: "no-cache",
-        credentials: "same-origin", // include, *same-origin, omit
         headers: {
-            'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
-            "Content-Type": "multipart/form-data"
+            'Access-Control-Allow-Origin' : 'http://localhost:9000'
         },
         body: formData,
 
-        // TODO: Investigar sobre como enviar varios archivos en un formData (LINK MARCADO EN GOOGLE)
+        // TODO: Documentar lo nuevo implementado
     })
 }
